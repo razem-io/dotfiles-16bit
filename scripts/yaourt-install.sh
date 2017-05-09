88d56c82751eb0d0bc8ca736d87af75c15b14d95
Julian Liebl <jujulian1987@gmail.com>
2017-05-09 13:26:11 +0200

    Create yaourt-install.sh

---
+++ scripts/yaourt-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -S base-devel
+cd /tmp
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+tar -xvzf package-query.tar.gz
+cd package-query
+makepkg -si
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
+tar -xvzf package-query.tar.gz
+cd package-query
+makepkg -si
