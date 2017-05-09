5a2796d2777f1379517849a4a771511f5a9bf316
Julian Liebl <jujulian1987@gmail.com>
2017-05-09 13:42:32 +0200

    Update install_yaourt.sh

--- scripts/install_yaourt.sh
+++ scripts/install_yaourt.sh
@@ -6,7 +6,8 @@ curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
 tar -xvzf package-query.tar.gz
 cd package-query
 makepkg -si
-curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
-tar -xvzf package-query.tar.gz
-cd package-query
+cd ..
+curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar -xvzf yaourt.tar.gz
+cd yaourt
 makepkg -si
