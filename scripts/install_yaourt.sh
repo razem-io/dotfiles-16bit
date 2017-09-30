93a3bd2b95415872f6d7751fd14e1d4f5881d151
Julian Liebl <jujulian1987@gmail.com>
2017-09-30 11:31:44 +0200

    fixed script to not ask for a confirm

--- scripts/install_yaourt.sh
+++ scripts/install_yaourt.sh
@@ -5,9 +5,9 @@ cd /tmp
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz
 tar -xvzf package-query.tar.gz
 cd package-query
-makepkg -si
+makepkg -si --no-confirm
 cd ..
 curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
 tar -xvzf yaourt.tar.gz
 cd yaourt
-makepkg -si
+makepkg -si --no-confirm
